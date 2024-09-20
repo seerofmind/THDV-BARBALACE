4ac62a478fe8bafe8f1dfe04f9f71730173fba11
Paula <barbalace.mapaula@gmail.com>
2024-09-20 20:10:41 -0300

    Decorations added
    Tables, Broken tables, Shelves, Broken shelves were added and placed in "tutorial" area

---
+++ TDHV - BARBALACE/Assets/triggerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Import scene management
+
+public class RestartGame : MonoBehaviour
+{
+    // This method will be called when the player enters the trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that triggered the collision is the player
+        if (other.CompareTag("Player"))
+        {
+            // Restart the game by reloading the current scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
