e9b070c326788588df8bef1fced1cd812bbf97e3
Paula <barbalace.mapaula@gmail.com>
2024-10-14 19:26:42 -0300

    Error solution try

--- TDHV - BARBALACE/Assets/Scripts/collectable.cs
+++ TDHV - BARBALACE/Assets/Scripts/collectable.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class collectable : MonoBehaviour
 {
@@ -9,13 +11,14 @@ public class collectable : MonoBehaviour
     public int score;
     [SerializeField] private List<GameObject> collectedItems;
     private List<GameObject> gears = new List<GameObject>();
-  
+    private Collider wallCollider;
     void Start()
     {
         objectRenderer = GetComponent<Renderer>();
         objectCollider = GetComponent<Collider>();
 
     }
+    
     void CollectItem(GameObject item)
     {
         collectedItems.Add(item); // Add the item to the list
@@ -37,13 +40,24 @@ public class collectable : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-           
+
             SetInvisibleAndDisableCollision();
             score = score + 1;
             Debug.Log(score);
         }
 
-    }
-}
-
+        if (Collision.CompareTag("Wall")) // Verificar si es la pared
+            {
+                if (score == 1) // Verifica si el puntaje es exactamente 1
+                {
+                    Debug.Log("Reiniciando la escena porque el jugador tiene 1 collectable.");
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+                else
+                {
+                    Debug.Log("No se reinicia la escena porque el puntaje no es exactamente 1.");
+                }
+            }
+        }
+}   
 
