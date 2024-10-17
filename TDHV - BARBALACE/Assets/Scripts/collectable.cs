dd6b3ab64252e426ce14264cdac46e1bdbcda6af
Paula <barbalace.mapaula@gmail.com>
2024-10-17 20:35:08 -0300

    Delete duplicated scripts

--- TDHV - BARBALACE/Assets/Scripts/collectable.cs
+++ TDHV - BARBALACE/Assets/Scripts/collectable.cs
@@ -1,36 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 
 public class collectable : MonoBehaviour
 {
     private Renderer objectRenderer;
     private Collider objectCollider;
     public int score;
-<<<<<<< HEAD
-
-=======
     [SerializeField] private List<GameObject> collectedItems;
     private List<GameObject> gears = new List<GameObject>();
-  
->>>>>>> parent of e9b070c (Error solution try)
+
     void Start()
     {
         objectRenderer = GetComponent<Renderer>();
         objectCollider = GetComponent<Collider>();
 
-<<<<<<< HEAD
-        
-=======
     }
-    
     void CollectItem(GameObject item)
     {
         collectedItems.Add(item); // Add the item to the list
         Debug.Log("Collected: " + item.name);
->>>>>>> parent of e9b070c (Error solution try)
     }
     void SetInvisibleAndDisableCollision()
     {
@@ -47,36 +36,14 @@ public class collectable : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-<<<<<<< HEAD
-            {
-                SetInvisibleAndDisableCollision();
-                score = score + 1;
-                Debug.Log(score);
-            }
-}
-}
-=======
         {
 
             SetInvisibleAndDisableCollision();
-            score = score + 1;
+            score = score + 10;
             Debug.Log(score);
         }
 
-        if (Collision.CompareTag("Wall")) // Verificar si es la pared
-            {
-                if (score == 1) // Verifica si el puntaje es exactamente 1
-                {
-                    Debug.Log("Reiniciando la escena porque el jugador tiene 1 collectable.");
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-                }
-                else
-                {
-                    Debug.Log("No se reinicia la escena porque el puntaje no es exactamente 1.");
-                }
-            }
-        }
-}   
+    }
+}
 
 
->>>>>>> parent of e9b070c (Error solution try)
