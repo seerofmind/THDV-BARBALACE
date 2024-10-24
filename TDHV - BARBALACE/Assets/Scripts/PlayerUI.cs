5bbe4c26e1a1d530464d4da64c55d8eddab9fedd
Paula <barbalace.mapaula@gmail.com>
2024-10-24 12:10:19 -0300

    Pause Menu Done

---
+++ TDHV - BARBALACE/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+
+public class PlayerUI : MonoBehaviour
+    
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] FirstPersonLook fpsLook;
+    private bool isInPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isInPause = !isInPause;
+            pauseMenu.SetActive(isInPause);
+        }
+        if (isInPause)
+        { 
+            Time.timeScale = 0;
+            fpsLook.enabled = false;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            fpsLook.enabled = true;
+        }
+    }
+}
