52724148a7dbab420124057dd514772d368fffc3
Paula <barbalace.mapaula@gmail.com>
2024-11-01 16:47:31 -0300

    Small changes for HidingPlaces

--- TDHV - BARBALACE/Assets/Scripts/PlayerUI.cs
+++ TDHV - BARBALACE/Assets/Scripts/PlayerUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Microsoft.Unity.VisualStudio.Editor;
+
 
 public class PlayerUI : MonoBehaviour
     
